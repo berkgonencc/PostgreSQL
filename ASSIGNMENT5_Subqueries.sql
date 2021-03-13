fcec5a1f9a7b62ef87b66a5a9999cd425f5387fa
berkgonencc <berkgonencc@gmail.com>
2021-03-13 15:00:38 +0300

    new commit

--- ASSIGNMENT5_Subqueries.sql
+++ ASSIGNMENT5_Subqueries.sql
@@ -1,3 +1,59 @@
+--TABLES
+create table students
+(
+	student_no integer,
+	student_name varchar(20),
+	age integer
+);
+
+insert into students values (1, 'Michael', 19);
+insert into students values (2, 'Doug', 18);
+insert into students values (3, 'Samantha', 21);
+insert into students values (4, 'Pete', 20);
+insert into students values (5, 'Ralph', 19);
+insert into students values (6, 'Arnold', 22);
+insert into students values (7, 'Michael', 19);
+insert into students values (8, 'Jack', 19);
+insert into students values (9, 'Rand', 17);
+insert into students values (10, 'Sylvia', 20);
+
+create table courses
+(
+	course_no varchar(5),
+	course_title varchar(20),
+	credits integer
+);
+
+insert into courses values ('CS110', 'Pre Calculus', 4);
+insert into courses values ('CS180', 'Physics', 4);
+insert into courses values ('CS107', 'Intro to Psychology', 3);
+insert into courses values ('CS210', 'Art History', 3);
+insert into courses values ('CS220', 'US History', 3);
+
+create table student_enrollment
+(
+	student_no integer,
+	course_no varchar(5)
+);
+
+insert into student_enrollment values (1, 'CS110');
+insert into student_enrollment values (1, 'CS180');
+insert into student_enrollment values (1, 'CS210');
+insert into student_enrollment values (2, 'CS107');
+insert into student_enrollment values (2, 'CS220');
+insert into student_enrollment values (3, 'CS110');
+insert into student_enrollment values (3, 'CS180');
+insert into student_enrollment values (4, 'CS220');
+insert into student_enrollment values (5, 'CS110');
+insert into student_enrollment values (5, 'CS180');
+insert into student_enrollment values (5, 'CS210');
+insert into student_enrollment values (5, 'CS220');
+insert into student_enrollment values (6, 'CS110');
+insert into student_enrollment values (7, 'CS110');
+insert into student_enrollment values (7, 'CS210');
+
+
+
 --Using subqueries only, write a SQL statement that returns the names of those students that are taking the courses Physics and US History.
 SELECT student_name 
 FROM students
